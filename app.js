ec13f49d8d44738e484c38a8ec9712d6a2245d4b
MarynaLapa <133876007+MarynaLapa@users.noreply.github.com>
2024-02-08 20:18:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,11 +17,21 @@ const invokeAction = async ({action, id, title, author}) => {
         case "removeById":
             const removeBook = await books.removeById(id);
             return console.log(removeBook);
+        default:
+            return console.log('Unknown action');
     }
 }
 
-invokeAction({ action: "read" })
+
+// invokeAction({ action: "read" })
 // invokeAction({action: "getById", id:"YxhM4QDxPeA3SmPHcdmfn"})
 //  invokeAction({action: "add", title:"Murder on the Orient Express", author: "Agatha Christie"})//Повторно використання потрібно змінити title i author
-// invokeAction({action: "updateById", id: "QUlkJbJYDNZ0XbnCsbyzc", title: "Death on the Nile", author: "Agatha Christie"})   
+// invokeAction({action: "updateById", id: "QUlkJbJYDNZ0XbnCsbyzc", title: "Death on the Nile", author: "Agatha Christie"})
 // invokeAction({ action: "removeById", id: "YxhM4QDxPeA3SmPHcdmfn" });
+
+const actionIndex = process.argv.indexOf("--action");
+if (actionIndex !== -1) {
+    const action = process.argv[actionIndex + 1];
+    invokeAction({ action });//node app --action read ( або || getById || updateById || removeById)
+}
+console.log('process.argv', process.argv)
